ad75dbcd1db78ce4df2b6241856dddf3694cf53c
Benjamin Thurston <benjamin.thurston87@gmail.com>
2017-11-16 19:18:29 -0500

    “Init_commit_Apelp_says_hi”

---
+++ apelp1.py
@@ -0,0 +1,16 @@
+#apelp mark 1
+#this is the start of something crazy.
+
+import datetime
+import os
+import random
+import datetime
+import webbrowser
+import time
+import calendar 
+
+# apelp greets you first with one of these greeetings \/ 
+greet = random.choice(("hi","hey","sup","whats up","hey it's jarvis","hello","hey there","good to see you"))
+print (greet)
+os.system("say '"+greet+"'") 
+resp1 = input("")
